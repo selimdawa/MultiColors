212aa49803363112a3207537960a2a9d364b7879
selimdawa <selimdawa@gmail.com>
2026-08-26 15:33:45 +0300

    Theme Colors - Update

--- multicolors/src/main/java/io/selimdawa/multicolors/ThemeRegistry.kt
+++ multicolors/src/main/java/io/selimdawa/multicolors/ThemeRegistry.kt
@@ -20,7 +20,6 @@ object ThemeRegistry {
             MultiColorTheme("EIGHT", R.string.mc_theme_eight, R.style.Theme_MC_Eight),
             MultiColorTheme("NINE", R.string.mc_theme_nine, R.style.Theme_MC_Nine),
             MultiColorTheme("GRADUAL_ONE", R.string.mc_theme_gradient_one, R.style.Theme_MC_Gradient_One),
-            MultiColorTheme("GRADUAL_TWO", R.string.mc_theme_gradient_two, R.style.Theme_MC_Gradient_Two),
             MultiColorTheme("GRADUAL_THREE", R.string.mc_theme_gradient_three, R.style.Theme_MC_Gradient_Three),
             MultiColorTheme("GRADUAL_FOUR", R.string.mc_theme_gradient_four, R.style.Theme_MC_Gradient_Four),
             MultiColorTheme("GRADUAL_FIVE", R.string.mc_theme_gradient_five, R.style.Theme_MC_Gradient_Five),
@@ -51,10 +50,18 @@ object ThemeRegistry {
             MultiColorTheme("G2_24", R.string.mc_theme_cool_sky, R.style.Theme_MC_G2_24),
             MultiColorTheme("G2_26", R.string.mc_theme_evening, R.style.Theme_MC_G2_26),
             MultiColorTheme("G2_28", R.string.mc_theme_kimoby, R.style.Theme_MC_G2_28),
+            MultiColorTheme("G2_29", R.string.mc_theme_kimoby_green, R.style.Theme_MC_G2_29),
+            MultiColorTheme("G2_31", R.string.mc_theme_kimoby_purple, R.style.Theme_MC_G2_31),
+            MultiColorTheme("G2_33", R.string.mc_theme_slate, R.style.Theme_MC_G2_33),
+            MultiColorTheme("G2_34", R.string.mc_theme_moss, R.style.Theme_MC_G2_34),
+            MultiColorTheme("G2_35", R.string.mc_theme_clay, R.style.Theme_MC_G2_35),
+            MultiColorTheme("G2_36", R.string.mc_theme_midnight, R.style.Theme_MC_G2_36),
+            MultiColorTheme("G2_37", R.string.mc_theme_dusty_blue, R.style.Theme_MC_G2_37),
+            MultiColorTheme("G2_38", R.string.mc_theme_olive, R.style.Theme_MC_G2_38),
+            MultiColorTheme("G2_39", R.string.mc_theme_rose, R.style.Theme_MC_G2_39),
 
             // 3-Color Gradients
-            MultiColorTheme("G3_2", R.string.mc_theme_neon_night, R.style.Theme_MC_G3_2),
-            MultiColorTheme("G3_10", R.string.mc_theme_neon_light, R.style.Theme_MC_G3_10),
+            MultiColorTheme("G3_2", R.string.mc_theme_neon, R.style.Theme_MC_G3_2),
             MultiColorTheme("G3_3", R.string.mc_theme_ocean_deep, R.style.Theme_MC_G3_3),
             MultiColorTheme("G3_5", R.string.mc_theme_sky, R.style.Theme_MC_G3_5),
             MultiColorTheme("G3_9", R.string.mc_theme_galaxy, R.style.Theme_MC_G3_9)
