70dbff479d0f57d338ffaac101cda02fc2e41ae9
selimdawa <selimdawa@gmail.com>
2026-08-29 06:59:04 +0300

    Edge to Edge - Add

--- app/src/main/java/com/flatcode/multicolors/MainActivity.kt
+++ app/src/main/java/com/flatcode/multicolors/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : BaseActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
--- app/src/main/java/com/flatcode/multicolors/TestActivity.kt
+++ app/src/main/java/com/flatcode/multicolors/TestActivity.kt
@@ -17,7 +17,7 @@ class TestActivity : BaseActivity() {
         binding = ActivityTestBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
